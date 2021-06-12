eacadd20f92f271985926a09e4cb8901fcbfd8ca
muhammedkarakul <muhammedkarakul@gmail.com>
2021-06-12 15:24:39 +0300

    App window created.

---
+++ 98791b35ec5bcff0ce73edc4a9858609/App/Delegate/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  98791b35ec5bcff0ce73edc4a9858609
+//
+//  Created by Muhammed Karakul on 12.06.2021.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        createWindow()
+        return true
+    }
+
+    private func createWindow() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
