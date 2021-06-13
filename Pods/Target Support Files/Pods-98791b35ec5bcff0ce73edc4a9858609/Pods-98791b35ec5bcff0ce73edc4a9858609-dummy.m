6213b7600a4054620b6ae90121e402c85ee8df4f
muhammedkarakul <muhammedkarakul@gmail.com>
2021-06-13 15:39:47 +0300

    no message

---
+++ Pods/Target Support Files/Pods-98791b35ec5bcff0ce73edc4a9858609/Pods-98791b35ec5bcff0ce73edc4a9858609-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_98791b35ec5bcff0ce73edc4a9858609 : NSObject
+@end
+@implementation PodsDummy_Pods_98791b35ec5bcff0ce73edc4a9858609
+@end
---
+++ Pods/Target Support Files/Pods-98791b35ec5bcff0ce73edc4a9858609/Pods-98791b35ec5bcff0ce73edc4a9858609-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_98791b35ec5bcff0ce73edc4a9858609VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_98791b35ec5bcff0ce73edc4a9858609VersionString[];
+
